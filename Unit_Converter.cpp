66a5e745ba7f1293b9995a63318c414624279638
Tobechukwu-Ikenwe <tobechukwu.ikenwe@usm.edu>
2025-12-27 15:48:28 +0100

    Refactor unit conversion logic into functions

--- Unit_Converter.cpp
+++ Unit_Converter.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 #include<cmath>
-
+#include<map>
 #include<string>
 
 using namespace std;
+double convertlength(double, char , char );
+double convertWeight(double , char , char );
+double convertTemperature(double , char, char);
+
 
 int main(){
     char unit;
@@ -16,6 +20,8 @@ int main(){
         return 1;
 
     }
+
+    
 
     if(unit == 'L'){
         char L_unit;
@@ -30,7 +36,7 @@ int main(){
         cout << "(M for Meters)  (K for Kilometers) (I for Inches) (m for miles)\n";
         cin >> L_converted_unit;
 
-        bool L_valid = ((L_unit == 'M' || L_unit == 'K' || L_unit == 'I' || L_unit == 'm') || (L_converted_unit == 'M' || L_converted_unit == 'K' || L_converted_unit == 'I' || L_converted_unit == 'm'));
+        bool L_valid = ((L_unit == 'M' || L_unit == 'K' || L_unit == 'I' || L_unit == 'm') && (L_converted_unit == 'M' || L_converted_unit == 'K' || L_converted_unit == 'I' || L_converted_unit == 'm'));
         if (!L_valid){
             cout << "Enter Valid unit for length\n";
             return 1;
@@ -41,105 +47,12 @@ int main(){
             return 1;
         }
 
-        //For meter unit selection
-
-        if (L_unit == 'M' && L_converted_unit == 'K'){
-            double L_converted_value;
-            L_converted_value = L_value / 1000;
-            cout << "Your new value is " << L_converted_value << "Km\n";
-
-        }
-
-        if (L_unit == 'M' && L_converted_unit == 'I'){
-            double L_converted_value;
-            L_converted_value = L_value * 39.37;
-            cout << "Your new value is " << L_converted_value << "In\n";
-            
-        }
-
-         if (L_unit == 'M' && L_converted_unit == 'm'){
-            double L_converted_value;
-            L_converted_value = L_value * 0.00062137;
-            cout << "Your new value is " << L_converted_value << "miles\n";
-            
-        }
-
-        //for kilometer unit selection
-
-         if (L_unit == 'K' && L_converted_unit == 'M'){
-            double L_converted_value;
-            L_converted_value = L_value * 1000;
-            cout << "Your new value is " << L_converted_value << "m\n";
-
-        }
-
-        if (L_unit == 'K' && L_converted_unit == 'I'){
-            double L_converted_value;
-            L_converted_value = L_value * 39,370.87;
-            cout << "Your new value is " << L_converted_value << "In\n";
-            
-        }
-
-         if (L_unit == 'K' && L_converted_unit == 'm'){
-            double L_converted_value;
-            L_converted_value = L_value * 0.62137;
-            cout << "Your new value is " << L_converted_value << "miles\n";
-            
-        }
-
-        //For Inches
-               if (L_unit == 'I' && L_converted_unit == 'M'){
-            double L_converted_value;
-            L_converted_value = L_value * 0.0254;
-            cout << "Your new value is " << L_converted_value << "m\n";
-
-        }
-
-        if (L_unit == 'I' && L_converted_unit == 'K'){
-            double L_converted_value;
-            L_converted_value = L_value * 0.0000254;
-            cout << "Your new value is " << L_converted_value << "km\n";
-            
-        }
-
-         if (L_unit == 'I' && L_converted_unit == 'm'){
-            double L_converted_value;
-            L_converted_value = L_value * 0.0000157828;
-            cout << "Your new value is " << L_converted_value << "miles\n";
-            
-        }
-
-        //For miles
-            if (L_unit == 'm' && L_converted_unit == 'M'){
-            double L_converted_value;
-            L_converted_value = L_value * 1609.344;
-            cout << "Your new value is " << L_converted_value << "m\n";
-
-        }
-
-        if (L_unit == 'm' && L_converted_unit == 'K'){
-            double L_converted_value;
-            L_converted_value = L_value * 1.609344;
-            cout << "Your new value is " << L_converted_value << "km\n";
-            
-        }
-
-         if (L_unit == 'm' && L_converted_unit == 'I'){
-            double L_converted_value;
-            L_converted_value = L_value * 63360;
-            cout << "Your new value is " << L_converted_value << "inches\n";
-            
-        }
-
-
-
-
-        
-        
-        
-
+        double result = convertlength( L_value, L_unit, L_converted_unit);
+        cout << "Your New Value is " << result <<  " " << L_converted_unit << "\n";
     }
 
+
+    //For weight
     if(unit == 'W'){
         char W_unit;
         char W_converted_unit;
@@ -147,71 +60,23 @@ int main(){
         cout << "Please select current unit: \n";
         cout << "(G for Grams)  (K for Kilograms) (P for Pounds)\n";
         cin >> W_unit;
+        cout << "Please select value of your unit: \n";
+        cin >> W_value;
         cout << "please select unit you would like to convert to: \n";
         cout << "(G  for Grams)  (K for Kilograms) (P for Pounds)\n";
         cin >> W_converted_unit;
 
-        bool W_valid = ((W_unit == 'G' || W_unit == 'K' || W_unit == 'P') || (W_converted_unit == 'G' || W_converted_unit == 'K' || W_converted_unit == 'P' ));
+        bool W_valid = ((W_unit == 'G' || W_unit == 'K' || W_unit == 'P') && (W_converted_unit == 'G' || W_converted_unit == 'K' || W_converted_unit == 'P' ));
         if (!W_valid){
             cout << "Enter Valid unit for Weight\n";
             return 1;
         }
-
-              //For Grams unit selection
-
-        if (W_unit == 'G' && W_converted_unit == 'K'){
-            double W_converted_value;
-            W_converted_value = W_value / 1000;
-            cout << "Your new value is " << W_converted_value << "Kg\n";
-
-        }
-
-        if (W_unit == 'G' && W_converted_unit == 'P'){
-            double W_converted_value;
-            W_converted_value = W_value * 0.00220462;
-            cout << "Your new value is " << W_converted_value << "lbs\n";
-            
-        }
-
-        
-
-        //for Pounds unit selection
-
-         if (W_unit == 'P' && W_converted_unit == 'G'){
-            double W_converted_value;
-            W_converted_value = W_value * 453.6;
-            cout << "Your new value is " << W_converted_value << "g\n";
-
-        }
-
-        if (W_unit == 'P' && W_converted_unit == 'K'){
-            double W_converted_value;
-            W_converted_value = W_value * 2.20462;
-            cout << "Your new value is " << W_converted_value << "Kg\n";
-            
-        }
+        double result =  convertWeight( W_value,  W_unit, W_converted_unit);
+        cout << "Your New Value is " << result <<  " " << W_converted_unit << "\n";
+    } 
 
 
-        //For Kilograms
-            if (W_unit == 'K' && W_converted_unit == 'G'){
-            double W_converted_value;
-            W_converted_value = W_value * 1000;
-            cout << "Your new value is " << W_converted_value << "g\n";
-
-        }
-
-        if (W_unit == 'K' && W_converted_unit == 'P'){
-            double W_converted_value;
-            W_converted_value = W_value /2.20462;
-            cout << "Your new value is " << W_converted_value << "lbs\n";
-            
-        }
-
-    
-       
-
-    }
-
+    //For Temperature
     if(unit == 'T'){
         char T_unit;
         char T_converted_unit;
@@ -219,37 +84,78 @@ int main(){
         cout << "Please select current unit: \n";
         cout << "(C for Celsius)  (F for Fahrenheit) \n";
         cin >> T_unit;
+        cout << "Please select value of your unit: \n";
+        cin >> T_value;
         cout << "please select unit you would like to convert to: \n";
         cout << "(C for Celsius)  (F for Fahrenheit) \n";
         cin >> T_converted_unit;
 
-        bool T_valid = ((T_unit == 'C' || T_unit == 'F' ) || (T_converted_unit == 'C' || T_converted_unit == 'F' ));
+        bool T_valid = ((T_unit == 'C' || T_unit == 'F' ) && (T_converted_unit == 'C' || T_converted_unit == 'F' ));
         if (!T_valid){
             cout << "Enter Valid unit for Temperature\n";
             return 1;
         }
 
-            //For Celsius unit selection
+        double result = convertTemperature(T_value, T_unit, T_converted_unit);
+        cout << "Your New Value is " << result << " " << T_converted_unit << "\n";
+}
+    
+}
+double convertlength(double L_value, char L_unit, char L_converted_unit){
+     //Create a Map to store the values of conversions from the Unit Meter to all other units
+        map<char, double> toMeter = {
+            {'M', 1.0},
+            {'K', 1000.0},
+            {'I', 0.0254},
+            {'m', 1609.347}
+       
+        };
 
-        if (T_unit == 'C' && T_converted_unit == 'F'){
-            double T_converted_value;
-            T_converted_value = (9/5 * T_value) + 32;
-            cout << "Your new value is " << T_converted_value << "F\n";
+         // create a variable meters to store the value converted to meters
+        double meters = L_value * toMeter.at(L_unit);
+        //Return the value in the unit we are converting to
+        return meters / toMeter.at(L_converted_unit);
 
-        }
+
+}
+
+double convertWeight(double W_value, char W_unit, char W_converted_unit){
+    //Create a Map to store the values of conversions from the Unit Grams to all other units
+    map<char, double> toGrams = {
+        {'G', 1.0},
+        {'K', 1000.0},
+        {'P', 453.6}
         
+    
 
-        //fOR Fahrenheit 
-        if (T_unit == 'F' && T_converted_unit == 'C'){
-            double T_converted_value;
-            T_converted_value = (5/9 * T_value) - 32;
-            cout << "Your new value is " << T_converted_value << "C\n";
-            
-        }
+    };
+    // create a variable grams to store the value converted to Grams
+    double grams = W_value * toGrams.at(W_unit);
+    //Return the value in the unit we are converting to
+    return grams / toGrams.at(W_converted_unit);
+}
+
+
+
+double convertTemperature(double T_value, char T_unit, char T_converted_unit){
+    //For Celsius unit selection
+
+    if (T_unit == 'C' && T_converted_unit == 'F'){
+        return (9.0/5.0 * T_value) + 32;
+
+    }
+    
+
+    //fOR Fahrenheit 
+    if (T_unit == 'F' && T_converted_unit == 'C'){
+        return (5.0/9.0) * (T_value - 32);
+        
+    }
 
        
     }
 
 
 
-};
+       
+        
